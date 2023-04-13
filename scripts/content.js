0b124399f9075398ff542c55edd9bd62a7c477cf
Konrad Miekus <miekus.konrad@gmail.com>
2023-04-13 07:29:35 +0200

    add html code to all iFrames through the plugin

--- scripts/content.js
+++ scripts/content.js
@@ -7,3 +7,10 @@ const handler = (evt) => {
 WATCHABLE_EVENTS.forEach((event) => {
   document.addEventListener(event, handler, true)
 })
+
+const para = document.createElement('p')
+para.style.setProperty('color', 'red')
+const node = document.createTextNode('Added by Chrome extension')
+para.appendChild(node)
+
+document.body.appendChild(para)
